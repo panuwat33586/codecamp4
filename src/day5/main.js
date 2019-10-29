1804f708cdb27675089d49f564fcf1d0ee1001c2
panuwat33586 <panuwat33586@gmail.com>
2019-10-29 17:34:34 +0700

    day5

--- src/day5/main.js
+++ src/day5/main.js
@@ -1,3 +1,19 @@
 export function main(arr) {
-	// Your code begins here;
+	let R=[]
+	let G=[]
+	let B=[]
+	for(let i=0;i<arr.length;i++){
+		if(arr[i]=="R"){
+			R.push(arr[i])
+		}if(arr[i]=="G"){
+            G.push(arr[i])
+		}if(arr[i]=="B"){
+            B.push(arr[i])
+		}
+	}
+
+	let RG=R.concat(G);
+	let RGB=RG.concat(B);
+	return RGB
 }
+

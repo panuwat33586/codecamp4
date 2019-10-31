7ba99eb8db5e866013f4ecfeec261f28b3d29d14
panuwat33586 <panuwat33586@gmail.com>
2019-10-31 21:45:04 +0700

    day 6-2

--- src/day6/main.js
+++ src/day6/main.js
@@ -1,5 +1,15 @@
 function main(root) {
-	// Your code begins here;
+	let node=root;
+	let queue=[node];
+	let finaldata=[]
+	while(queue.length){
+		node=queue.shift()
+		if(node.left)queue.push(node.left)
+		if(node.right)queue.push(node.right)
+		finaldata.push(node.value)
+	}
+let result=finaldata.sort(function(a,b){return a - b})
+return result[result.length-2]
 }
 
 module.exports = { main };
--- src/day7/main.js
+++ src/day7/main.js
@@ -1,5 +1,24 @@
 function main(arr) {
-	// Your code begins here;
-}
+	let x=[];
+	x.push([]);
+	for(let i=0;i<arr.length;i++){
+		x.push([arr[i]])
+	};for(let i=0;i<arr.length;i++){
+		if(arr.length>3){
 
+	}
+		for(let j=i+1;j<arr.length;j++){
+			x.push([arr[i],arr[j]]);
+		}
+	}
+	if(x.length<2**arr.length){
+		x.push(arr);
+		return x
+	}else{
+		return x
+	}
+	}
+
+console.log(main([1, 2, 3, 4]))
+    
 module.exports = { main };
